8613afc0cde316a8a4cf94277f8c1c516233a1d5
Kevin Bernfeld <kcbernfeld@gmail.com>
2016-06-22 17:20:32 -0400

    initial commit i guess

--- Lorebot/Program.fs
+++ Lorebot/Program.fs
@@ -1,7 +1,46 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System.Net.Http
+open WebSocketSharp
+open System
+open Types
+open Newtonsoft.Json
+open System.Text
+open System.Linq
+
+let messageParse arg (users : User list) (ws : WebSocket) = 
+    let message = JsonConvert.DeserializeObject<SlackMessage>(arg)
+    
+    let getUser user = 
+        let item = users.Single(fun x -> x.id = user)
+        item.name
+
+    match message.``type`` with
+    | "message" -> 
+        match message.text.StartsWith("@myopicm") with
+        | true -> printfn "%A WANTS YOUR ATTENTION: %A" (getUser message.user) message.text
+        | false -> printfn "%A: %A" (getUser message.user) message.text
+    | "user_typing" -> ()
+    | "channel_marked" -> ()
+    | "presence_change" -> ()
+    | "reconnect_url" -> ()
+    | _ -> printfn "%A: %A" message.user message.``type``
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    use client = new HttpClient()
+    client.BaseAddress <- Uri("https://slack.com/")
+    let api = "api/rtm.start?token=xoxp-13038755555-13039040003-53411645204-804d159ae0&no_unreads=true&pretty=1"
+    let responseTask = client.PostAsync(api, new StringContent(""))
+    responseTask.Wait()
+
+    let response = responseTask.Result
+    let r = response.Content.ReadAsStringAsync()
+    r.Wait()
+
+    let sr = JsonConvert.DeserializeObject<SlackBotResponse>(r.Result)
+
+    use ws = new WebSocket(sr.url)
+    ws.Connect()
+    ws.OnMessage.Add(fun x -> (messageParse x.Data sr.users ws))
+
+    Console.ReadKey(true) |> ignore
+    0
---
+++ Lorebot/Types.fs
@@ -0,0 +1,67 @@
+﻿module Types
+
+open System.Collections.Generic
+open System
+
+type Self = {
+    id : string
+    name : string
+    prefs : Dictionary<Object, Object>
+    created : string
+}
+
+type Team = {
+    id : string
+    name : string
+    email_domain : string
+    domain : string
+    icon : Dictionary<Object, Object>
+    msg_edit_window_mins : int
+    over_storage_limit : bool
+    prefs : Dictionary<Object, Object>
+    plan : string
+}
+
+type User = {
+    id : string
+    name : string
+}
+
+
+type SlackBotResponse = {
+    ok : bool
+    url : string
+    users : User list
+}
+
+type Webhook = {
+    url : string
+    channel : string
+    configuration_url : string
+}
+
+type Bot = {
+    bot_user_id : string
+    bot_access_token : string
+}
+
+type SlackAuthResponse = {
+    access_token : string
+    scope : string
+    team_name : string
+    team_id : string
+    incoming_webhook : Webhook
+    bot : Bot
+}
+
+type SlackAuthRequest = {
+    token : string
+    no_unreads : bool
+}
+
+type SlackMessage = {
+    text : string
+    channel : string
+    user : string
+    ``type`` : string
+}
